0dbf5ef728d8c710180e75493943311b81d9c090
ramkumar07 <ramkumarramkumar55922@gmail.com>
2026-08-03 02:54:41 +0530

    Remove unused Map import

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,48 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import About from "../components/About";
 import Services from "../components/Services";
-import Contact from "../components/Contact";
-import Map from "../components/LocationMap";
-import LocationMap from "../components/LocationMap";
 import Testimonials from "../components/Testimonials";
 import Appointment from "../components/Appointment";
+import Contact from "../components/Contact";
+import LocationMap from "../components/LocationMap";
+import Footer from "../components/Footer";
+import FloatingButtons from "../components/FloatingButtons";
 
 export default function Home() {
   return (
     <main className="bg-[#0b0613] text-white">
       <Navbar />
-      <Hero />
-      <About />
-      <Services />
-      <Testimonials />
-      <Appointment />
-      <Contact />
-      <LocationMap />
+
+      <section id="home">
+        <Hero />
+      </section>
+
+      <section id="about">
+        <About />
+      </section>
+
+      <section id="services">
+        <Services />
+      </section>
+
+      <section id="testimonials">
+        <Testimonials />
+      </section>
+
+      <section id="appointment">
+        <Appointment />
+      </section>
+
+      <section id="contact">
+        <Contact />
+      </section>
+
+      <section id="location">
+        <LocationMap />
+      </section>
+
+      <Footer />
+      <FloatingButtons />
     </main>
   );
 }
